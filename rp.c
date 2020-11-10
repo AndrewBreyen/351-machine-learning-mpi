61c25ecde29a0c7811a658871d3af4151cd9d509
ABREYEN001 <abreyen001@csbsju.edu>
2020-11-10 15:45:57 -0600

    End of class Nov 10, adds debugging prints

--- rp.c
+++ rp.c
@@ -23,6 +23,9 @@ THE SOFTWARE.
 /* assert */
 #include <assert.h>
 
+/* MPI API */
+#include <mpi.h>
+
 /* fabs */
 #include <math.h>
 
@@ -31,6 +34,8 @@ THE SOFTWARE.
 
 /* EXIT_SUCCESS, malloc, calloc, free, qsort */
 #include <stdlib.h>
+
+#define MPI_SIZE_T MPI_UNSIGNED_LONG
 
 struct distance_metric {
   size_t viewer_id;
@@ -49,40 +54,110 @@ cmp(void const *ap, void const *bp)
 int
 main(int argc, char * argv[])
 {
+  int ret, rank, p;
   size_t n, m, k;
+
+  ret = MPI_Init(&argc, &argv);
+  assert(MPI_SUCCESS == ret);
 
   /* Validate command line arguments. */
   assert(2 == argc);
 
-  /* ... */
-  char const * const fn = argv[1];
+  ret = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  assert(MPI_SUCCESS == ret);
 
-  /* Validate input. */
-  assert(fn);
+  ret = MPI_Comm_size(MPI_COMM_WORLD, &p);
+  assert(MPI_SUCCESS == ret);
 
-  /* Open file. */
-  FILE * const fp = fopen(fn, "r");
-  assert(fp);
+  double * rating;
 
-  /* Read file. */
-  fscanf(fp, "%zu %zu", &n, &m);
+  if (0 == rank) {
+    /* ... */
+    char const * const fn = argv[1];
 
-  /* Allocate memory. */
-  double * const rating = malloc(n * m * sizeof(*rating));
+    /* Validate input. */
+    assert(fn);
 
-  /* Check for success. */
-  assert(rating);
+    /* Open file. */
+    FILE * const fp = fopen(fn, "r");
+    assert(fp);
 
-  for (size_t i = 0; i < n; i++) {
-    for (size_t j = 0; j < m; j++) {
-      fscanf(fp, "%lf", &rating[i * m + j]);
+    /* Read file. */
+    fscanf(fp, "%zu %zu", &n, &m);
+
+    /* Allocate memory. */
+    rating = malloc(n * m * sizeof(*rating));
+
+    /* Check for success. */
+    assert(rating);
+
+    for (size_t i = 0; i < n; i++) {
+      for (size_t j = 0; j < m; j++) {
+        fscanf(fp, "%lf", &rating[i * m + j]);
+      }
+    }
+
+    /* Close file. */
+    ret = fclose(fp);
+    assert(!ret);
+
+    for (int r = 1; r < p; r++) {
+      printf("[%d->%d] (%zu) %zu\n", rank, r, 1, n);
+      ret = MPI_Send(&n, 1, MPI_SIZE_T, r, 0, MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+      printf("[%d->%d] (%zu) %zu\n", rank, r, 1, m);
+      ret = MPI_Send(&m, 1, MPI_SIZE_T, r, 0, MPI_COMM_WORLD);
+      assert(MPI_SUCCESS == ret);
+
+      size_t nrows;
+      size_t const base = 1 + ((n - 1) / p); // ceil(n / p)
+      if (r == p - 1) {
+        nrows = n - r * base;
+      } else {
+        nrows = base;
+      }
+
+      printf(">>> [%d->%d] (%zu)\n", rank, r, nrows);
+      ret = MPI_Send(&(rating[r * base * m]), nrows * m, MPI_DOUBLE, r, 0, MPI_COMM_WORLD);
+      //print out what rating is being sent
+      printf("Sending rating: %lf to thread %d\n", rating[r * base * m], r);
+      assert(MPI_SUCCESS == ret);
+    }
+  } else {
+    ret = MPI_Recv(&n, 1, MPI_SIZE_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    assert(MPI_SUCCESS == ret);
+    printf("[%d<-%d] (%zu) %zu\n", rank, 0, 1, n);
+    ret = MPI_Recv(&m, 1, MPI_SIZE_T, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    assert(MPI_SUCCESS == ret);
+    printf("[%d<-%d] (%zu) %zu\n", rank, 0, 1, m);
+
+    size_t nrows;
+    size_t const base = 1 + ((n - 1) / p); // ceil(n / p)
+    if (rank == p - 1) {
+      nrows = n - rank * base;
+    } else {
+      nrows = base;
+    }
+
+    /* Allocate memory. */
+    rating = malloc(nrows * m * sizeof(*rating));
+
+    /* Check for success. */
+    assert(rating);
+
+    ret = MPI_Recv(rating, nrows * m, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    assert(MPI_SUCCESS == ret);
+    printf("[%d<-%d] (%zu)\n", rank, 0, nrows * m);
+
+
+    for (int j = 0; j<nrows; j++){
+      for (int i = 0; i<m; i++){
+        printf("I am thread %d. Recieving rating: %lf from thread 0\n", rank, rating[j*m+i]);
+      }
     }
   }
 
-  /* Close file. */
-  int const ret = fclose(fp);
-  assert(!ret);
-
+  #if 0
   /* Allocate more memory. */
   double * const urating = malloc(m * sizeof(*urating));
 
@@ -91,55 +166,59 @@ main(int argc, char * argv[])
 
   /* Get user input. */
   for (size_t j = 0; j < m - 1; j++) {
-    printf("Enter your rating for movie %zu: ", j + 1);
-    scanf("%lf", &urating[j]);
-  }
+  printf("Enter your rating for movie %zu: ", j + 1);
+  scanf("%lf", &urating[j]);
+}
 
-  /* Allocate more memory. */
-  struct distance_metric * const distance = calloc(n, sizeof(*distance));
+/* Allocate more memory. */
+struct distance_metric * const distance = calloc(n, sizeof(*distance));
 
-  /* Check for success. */
-  assert(distance);
+/* Check for success. */
+assert(distance);
 
-  /* Compute distances. */
-  for (size_t i = 0; i < n; i++) {
-    distance[i].viewer_id = i;
-    for (size_t j = 0; j < m - 1; j++) {
-      distance[i].distance += fabs(urating[j] - rating[i * m + j]);
-    }
-  }
+/* Compute distances. */
+for (size_t i = 0; i < n; i++) {
+distance[i].viewer_id = i;
+for (size_t j = 0; j < m - 1; j++) {
+distance[i].distance += fabs(urating[j] - rating[i * m + j]);
+}
+}
 
-  /* Sort distances. */
-  qsort(distance, n, sizeof(*distance), cmp);
+/* Sort distances. */
+qsort(distance, n, sizeof(*distance), cmp);
 
-  /* Get user input. */
-  printf("Enter the number of similar viewers to report: ");
-  scanf("%zu", &k);
+/* Get user input. */
+printf("Enter the number of similar viewers to report: ");
+scanf("%zu", &k);
 
-  /* Output k viewers who are least different from the user. */
-  printf("Viewer ID   Movie five   Distance\n");
-  printf("---------------------------------\n");
+/* Output k viewers who are least different from the user. */
+printf("Viewer ID   Movie five   Distance\n");
+printf("---------------------------------\n");
 
-  for (size_t i = 0; i < k; i++) {
-    printf("%9zu   %10.1lf   %8.1lf\n", distance[i].viewer_id + 1,
-      rating[distance[i].viewer_id * m + 4], distance[i].distance);
-  }
+for (size_t i = 0; i < k; i++) {
+printf("%9zu   %10.1lf   %8.1lf\n", distance[i].viewer_id + 1,
+rating[distance[i].viewer_id * m + 4], distance[i].distance);
+}
 
-  printf("---------------------------------\n");
+printf("---------------------------------\n");
 
-  /* Compute the average to make the prediction. */
-  double sum = 0.0;
-  for (size_t i = 0; i < k; i++) {
-    sum += rating[distance[i].viewer_id * m + 4];
-  }
+/* Compute the average to make the prediction. */
+double sum = 0.0;
+for (size_t i = 0; i < k; i++) {
+sum += rating[distance[i].viewer_id * m + 4];
+}
 
-  /* Output prediction. */
-  printf("The predicted rating for movie five is %.1lf.\n", sum / k);
+/* Output prediction. */
+printf("The predicted rating for movie five is %.1lf.\n", sum / k);
 
-  /* Deallocate memory. */
-  free(rating);
-  free(urating);
-  free(distance);
+/* Deallocate memory. */
+free(rating);
+free(urating);
+free(distance);
+#endif
 
-  return EXIT_SUCCESS;
+ret = MPI_Finalize();
+assert(MPI_SUCCESS == ret);
+
+return EXIT_SUCCESS;
 }
